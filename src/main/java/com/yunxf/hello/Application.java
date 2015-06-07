c6d9bffd4879048399db05b771fff8382d523132
yunxiaofeng <yunxiaofeng1001@126.com>
2015-06-07 17:42:06 +0800

    试一下

---
+++ src/main/java/com/yunxf/hello/Application.java
@@ -0,0 +1,11 @@
+package com.yunxf.hello;
+
+/**
+ * Created by yunxiaofeng on 2015/6/7.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("HelloDemo");
+        System.out.println("hehehe");
+    }
+}
